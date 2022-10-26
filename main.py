738cbd4cebaac025bbdd7204706c5aea41f0be6c
JanGospodarek <jan.gospo@onet.pl>
2022-10-26 11:57:52 +0200

    play again

--- main.py
+++ main.py
@@ -1,31 +1,42 @@
 import random
 choices=['rock','paper','scissors']
-computer=random.choice(choices)
-player=None
-while player not in choices:
-    text=input('rock, paper or scissors? ')
-    player=text.lower()
-def output(result):
-    print('computer: ',computer)
-    print('player: ',player)
-    if result=='tie': print('tie!')
-    elif result=="win": print('You win!')
-    elif result=="lost": print('You lose!')
 
-if player ==computer:
-    output('tie')
-elif player=='rock':
-    if computer=='paper':
-        output("lose")
-    else:
-        output('win')
-elif player=='scissors':
-    if computer=='rock':
-        output("lose")
-    else:
-        output('win')
-elif player=='paper':
-    if computer=='scissors':
-        output("lose")
-    else:
-        output('win')
+
+
+while True:
+    computer = random.choice(choices)
+    player = None
+    def output(result):
+        print('computer: ', computer)
+        print('player: ', player)
+        if result == 'tie':
+            print('tie!')
+        elif result == "win":
+            print('You win!')
+        elif result == "lost":
+            print('You lose!')
+    while player not in choices:
+        text=input('rock, paper or scissors? ')
+        player=text.lower()
+
+    if player ==computer:
+       output('tie')
+    elif player=='rock':
+        if computer=='paper':
+            output("lose")
+        else:
+          output('win')
+    elif player=='scissors':
+        if computer=='rock':
+            output("lose")
+        else:
+            output('win')
+    elif player=='paper':
+        if computer=='scissors':
+            output("lose")
+        else:
+            output('win')
+    play_again=input('play again? (Y/N)').lower()
+    if play_again!='y':
+        break
+print('bye!')
